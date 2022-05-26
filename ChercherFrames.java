1eeefceaa1275aac659e6dad7e4f10c9b9653f98
RAVIND1719 <99536592+RAVIND1719@users.noreply.github.com>
2022-05-26 15:04:03 +0530

    Update ChercherFrames.java

--- ChercherFrames.java
+++ ChercherFrames.java
@@ -1,1 +1,39 @@
+package assign_w4_d2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChercherFrames {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://chercher.tech/practice/frames-example-selenium-webdriver");
+		driver.manage().window().maximize();
+
+		driver.switchTo().frame("frame2");
+
+		driver.findElement(By.xpath("//select[@id='animals']")).sendKeys("Avatar");
+
+		driver.switchTo().defaultContent();
+
+		driver.switchTo().frame("frame1");
+
+		driver.findElement(By.xpath("//input")).sendKeys("Frames");
+
+		driver.switchTo().frame("frame3");
+
+		driver.findElement(By.xpath("//input[@id='a']")).click();
+
+		driver.switchTo().parentFrame();
+
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("//input")).clear();
+
+		driver.findElement(By.xpath("//input")).sendKeys("Moved to parent Frame");
+	}
+
+}
